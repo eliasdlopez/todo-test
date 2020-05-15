d437334d11c0384fc262e8c6211e3f3f98191760
Elias <eliasdamianlopez@gmail.com>
2020-05-14 21:01:35 -0500

    solucionado

--- src/App.js
+++ src/App.js
@@ -11,28 +11,57 @@ import Filter from "./components/filter";
 
 export default class App extends Component {
   state = {
-    list: data
+    list: data,
+    search: ''
   };
 
   // Should change todo to completed or incompleted
   onItemPress = todo => {
     console.log(todo);
+    let modifiedTasks = data.map((val, i) => {
+      if (i === todo.id -1) {
+        val.completed = !val.completed
+      }
+      return val;
+    })
+    this.setState({
+      list: modifiedTasks
+    })
   };
 
   // Should filter items using filter value
   onFilterchange = filterValue => {
-    console.log(filterValue);
+    if (filterValue) {
+      this.setState({
+        search: filterValue
+      })
+      console.log(filterValue)
+    }
   };
 
   render() {
     const { onFilterchange, onItemPress, state } = this;
     const { list } = state;
 
+    let searchString = this.state.search.trim().toLowerCase();
+    
+    let filteredTasks = data.filter(function(i) {
+      return i.title.toLowerCase().includes( searchString );
+      });
+
+    // if (searchString.length > 0) {
+      
+    //   filteredTasks = data.filter(function(i) {
+    //   return i.title.toLowerCase().includes( searchString );
+    //   });
+    // }
+    
+
     return (
       <div className="App">
         <Header />
         <Filter onChange={onFilterchange} />
-        {list.map(item => (
+        {filteredTasks.map(item => (
           <Item onPress={onItemPress} key={item.id} {...item} />
         ))}
       </div>
